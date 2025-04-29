b26447ddcffe7d9c8876523de650ddff28679793
Radwa Amr <111903049+Radwaamr@users.noreply.github.com>
2025-04-30 00:31:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import numpy as np
 import joblib
 
-# تحميل النموذج
+
 model = joblib.load("concrete_model.pkl")
 
 st.title("Concrete Compressive Strength Prediction")
 
-# إدخال القيم
+
 cement = st.number_input("Cement (kg/m^3)", min_value=0.0)
 slag = st.number_input("Blast Furnace Slag", min_value=0.0)
 fly_ash = st.number_input("Fly Ash", min_value=0.0)
